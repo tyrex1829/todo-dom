b31da60c4a4521387054d6bf6165fa701c41ed04
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-12 12:53:53 +0530

    using backend api for data

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-let globalId = 1;
-
-function markAsDone(todoId){
-    const parent = document.getElementById(todoId);
-    parent.children[3].innerHTML = "Done!";
-}
 
 function createChild(title, desc, id){
     const outerDiv = document.createElement("div");
@@ -26,16 +20,74 @@ function createChild(title, desc, id){
     return outerDiv;
 }
 
-function addTodo(){
-    const title = document.querySelector("#title");
-    const desc = document.querySelector("#description");
+function updateDomAccToState(state){
     const parent = document.querySelector(".container");
-
-    if(title.value !== "" && desc.value !== ""){
-        parent.appendChild(createChild(title.value, desc.value, globalId++));
+    parent.innerHTML = "";
+    for(let i = 0; i < state.length; i++){
+        const child = createChild(state[i].title, state[i].description, state[i].id,);
+        parent.appendChild(child);
     }
-
-    title.value = "";
-    desc.value = "";
-
 }
+window.setInterval(async function(){
+    const res = await fetch("https://sum-server.100xdevs.com/todos");
+    const json = await res.json();
+    updateDomAccToState(json.todos);
+}, 5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let globalId = 1;
+
+// function markAsDone(todoId){
+//     const parent = document.getElementById(todoId);
+//     parent.children[3].innerHTML = "Done!";
+// }
+
+// function createChild(title, desc, id){
+//     const outerDiv = document.createElement("div");
+//     const innerDiv1 = document.createElement("div");
+//     const innerDiv2 = document.createElement("div");
+//     const buttonInDiv = document.createElement("button");
+//     const brTag = document.createElement("br");
+
+//     innerDiv1.innerHTML = title;
+//     innerDiv2.innerHTML = desc;
+//     buttonInDiv.innerHTML = "Mark as done";
+//     buttonInDiv.setAttribute("onclick", `markAsDone(${id})`);
+
+//     outerDiv.append(brTag);
+//     outerDiv.append(innerDiv1);
+//     outerDiv.append(innerDiv2);
+//     outerDiv.append(buttonInDiv);
+//     outerDiv.setAttribute("id", id);
+
+//     return outerDiv;
+// }
+
+// function addTodo(){
+//     const title = document.querySelector("#title");
+//     const desc = document.querySelector("#description");
+//     const parent = document.querySelector(".container");
+
+//     if(title.value !== "" && desc.value !== ""){
+//         parent.appendChild(createChild(title.value, desc.value, globalId++));
+//     }
+
+//     title.value = "";
+//     desc.value = "";
+
+// }
